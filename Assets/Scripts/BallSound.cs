2121c7de8f4c39647f2d28fcbe12920179243b39
Gabriele Lopez <gabriele.ope@gmail.com>
2025-09-01 19:40:07 +0200

    added multiple sounds for ball collison

--- Assets/Scripts/BallSound.cs
+++ Assets/Scripts/BallSound.cs
@@ -1,9 +1,10 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BallSound : MonoBehaviour
 {
-    public AudioClip ballSound;
+    public AudioClip[] ballSounds;
     private AudioSource _audioSource;
     private Rigidbody _rigidbody;
 
@@ -11,12 +12,13 @@ public class BallSound : MonoBehaviour
     {
         _audioSource = GetComponent<AudioSource>();
         _rigidbody = GetComponent<Rigidbody>();
-        _audioSource.clip = ballSound;
+        _audioSource.clip = ballSounds[0];
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        _audioSource.volume = Mathf.Clamp01(other.relativeVelocity.magnitude / 20);
+        _audioSource.clip = ballSounds[Random.Range(0, ballSounds.Length)];
+        _audioSource.volume = Mathf.Clamp01(other.relativeVelocity.magnitude / 20 * 2f);
         _audioSource.Play();
     }
 }
